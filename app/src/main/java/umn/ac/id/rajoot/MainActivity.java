928c7d9402e0dfa04192202c51f0d5e62178bfce
Fadla Ichsan <fadlaichsann@gmail.com>
2021-04-25 11:32:10 +0700

    Update Information Activity & Location

--- app/src/main/java/umn/ac/id/rajoot/MainActivity.java
+++ app/src/main/java/umn/ac/id/rajoot/MainActivity.java
@@ -2,31 +2,143 @@ package umn.ac.id.rajoot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
     BottomNavigationView bottomNavigationView;
     public static final int TIME_INTERVAL = 2000;
     private long backPressed;
+
+    TextView tvCity;
+    LocationManager locationManager;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getLocation();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        grantPermission();
+
+        tvCity = findViewById(R.id.lokasi_kota);
+
+        // Hide actionbar this splashscreen
+        getSupportActionBar().hide();
+
+        checkLocationIsEnabledOrNot(); //this will redirect us to the location setting
+
         // Set default first fragment in main activity is home fragment
         bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
+
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 5, (LocationListener) this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkLocationIsEnabledOrNot() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsEnabled = false;
+        boolean networkEnabled = false;
+
+        try {
+            gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (!gpsEnabled && !networkEnabled) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Enable GPS Service")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //this intent redirect us to the location settings, if GPS is disabled this dialog will be show
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    }).setNegativeButton("Cancel", null)
+                    .show();
+        }
+
+    }
+
+    private void grantPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        try {
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            tvCity.setText(addresses.get(0).getSubAdminArea());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
     }
 
     // Adjust the fragment when item bottom navigation checked
@@ -57,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
     // Method for go to sell activity when item sell checked
     public void goToSellActivity() {
         startActivity(new Intent(getApplicationContext(), SellActivity.class));
-        overridePendingTransition(0,0);
+        overridePendingTransition(0, 0);
     }
 
     @Override
@@ -69,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             return;
         } else {
-            Toast.makeText(MainActivity.this,"Press Back Again!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Press Back Again!", Toast.LENGTH_SHORT).show();
         }
 
         backPressed = System.currentTimeMillis();
--- app/src/main/java/umn/ac/id/rajoot/ProfileFragment.java
+++ app/src/main/java/umn/ac/id/rajoot/ProfileFragment.java
@@ -1,13 +1,26 @@
 package umn.ac.id.rajoot;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +40,10 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class ProfileFragment extends Fragment {
     LinearLayout itemCraftsForSale;
     LinearLayout itemRatingApp;
@@ -38,6 +55,15 @@ public class ProfileFragment extends Fragment {
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firebaseFirestore;
     String userID;
+
+//    TextView tvCity;
+//    LocationManager locationManager;
+//
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        getLocation();
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -55,6 +81,12 @@ public class ProfileFragment extends Fragment {
         firebaseAuth        = FirebaseAuth.getInstance();
         firebaseFirestore   = FirebaseFirestore.getInstance();
         userID              = firebaseAuth.getCurrentUser().getUid();
+
+//        grantPermission();
+//
+//        tvCity = view.findViewById(R.id.lokasi_kota);
+//
+//        checkLocationIsEnabledOrNot(); //this will redirect us to the location setting
 
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
@@ -135,6 +167,82 @@ public class ProfileFragment extends Fragment {
 
         return view;
     }
+
+//    private void getLocation() {
+//        try {
+//            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+//            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 5, (LocationListener) this);
+//        } catch (SecurityException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void checkLocationIsEnabledOrNot() {
+//        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+//        boolean gpsEnabled = false;
+//        boolean networkEnabled = false;
+//
+//        try {
+//            gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        if (!gpsEnabled && !networkEnabled) {
+//            new AlertDialog.Builder(getContext())
+//                    .setTitle("Enable GPS Service")
+//                    .setCancelable(false)
+//                    .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            //this intent redirect us to the location settings, if GPS is disabled this dialog will be show
+//                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+//                        }
+//                    }).setNegativeButton("Cancel", null)
+//                    .show();
+//        }
+//
+//    }
+//
+//    private void grantPermission() {
+//        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//                && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+//        }
+//    }
+//
+//    @Override
+//    public void onLocationChanged(@NonNull Location location) {
+//        try {
+//            Geocoder geocoder = new Geocoder(getActivity().getApplicationContext(), Locale.getDefault());
+//            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+//
+//            tvCity.setText(addresses.get(0).getSubAdminArea());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderEnabled(@NonNull String provider) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderDisabled(@NonNull String provider) {
+//
+//    }
 
     // When the button daftar clicked then go to the crafts for sale activity
     public void goToCraftForSale() {
